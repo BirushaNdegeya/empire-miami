b36d6f510c4ff368e8231a82cac35e810e9b950b
BirushaNdegeya <birushandegeya@gmail.com>
2025-01-20 19:25:02 +0200

    fix: fix services logo

--- components/service.tsx
+++ components/service.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
 import Image from "next/image";
 
 interface Service {
@@ -16,14 +16,13 @@ const services: Service[] = [
     description:
       "Impression grand format sur bâches, banderoles, et autres supports publicitaires.",
     icon: (
-      <div className="bg-primary p-3 rounded-lg">
-        <Image
-          src={require("../public/icons/all-supports.jpeg")}
-          alt="All supports icon"
-          width={200}
-          height={200}
-        />
-      </div>
+      <Image
+        src={require("../public/icons/all-supports.jpeg")}
+        alt="All supports icon"
+        // className="w-full"
+        // width={380}
+        // height={200}
+      />
     ),
   },
   {
@@ -32,14 +31,12 @@ const services: Service[] = [
     description:
       "Impression sur T-shirts, polos (Lacoste) et autres produits sublimés.",
     icon: (
-      <div className="bg-primary p-3 rounded-lg">
-        <Image
-          src={require("../public/icons/target.jpeg")}
-          alt="Personalization icon"
-          width={200}
-          height={200}
-        />
-      </div>
+      <Image
+        src={require("../public/icons/target.jpeg")}
+        alt="Personalization icon"
+        // width={200}
+        // height={200}
+      />
     ),
   },
   {
@@ -48,14 +45,12 @@ const services: Service[] = [
     description:
       "Réalisées en interne pour garantir des designs uniques et de haute qualité.",
     icon: (
-      <div className="bg-primary p-3 rounded-lg">
-        <Image
-          src={require("../public/icons/graphic.jpeg")}
-          alt="Graphic design icon"
-          width={200}
-          height={200}
-        />
-      </div>
+      <Image
+        src={require("../public/icons/graphic.jpeg")}
+        alt="Graphic design icon"
+        // width={200}
+        // height={200}
+      />
     ),
   },
   {
@@ -63,14 +58,12 @@ const services: Service[] = [
     title: "Développement numérique",
     description: "Développement de sites web, applications mobiles et desktop.",
     icon: (
-      <div className="bg-primary p-3 rounded-lg">
-        <Image
-          src={require("../public/icons/development.jpeg")}
-          alt="Development icon"
-          width={200}
-          height={200}
-        />
-      </div>
+      <Image
+        src={require("../public/icons/development.jpeg")}
+        alt="Development icon"
+        // width={200}
+        // height={200}
+      />
     ),
   },
   {
@@ -79,14 +72,12 @@ const services: Service[] = [
     description:
       "Installation et configuration de machines avec systèmes d'exploitation.",
     icon: (
-      <div className="bg-primary p-3 rounded-lg">
-        <Image
-          src={require("../public/icons/computer.jpeg")}
-          alt="System installation icon"
-          width={200}
-          height={200}
-        />
-      </div>
+      <Image
+        src={require("../public/icons/computer.jpeg")}
+        alt="System installation icon"
+        // width={200}
+        // height={200}
+      />
     ),
   },
   {
@@ -95,15 +86,13 @@ const services: Service[] = [
     description:
       "Photos passeport, cartes de service, invitations, stickers, autocollants, etc.",
     icon: (
-      <div className="bg-primary p-3 rounded-lg">
-        <Image
-          src={require("../public/icons/documents.jpeg")}
-          alt="Document printing icon"
-          className="w-[200px] h-[200px] object-cover"
-          width={200}
-          height={200}
-        />
-      </div>
+      <Image
+        src={require("../public/icons/documents.jpeg")}
+        alt="Document printing icon"
+        // className="w-[200px] h-[200px] object-cover"
+        // width={200}
+        // height={200}
+      />
     ),
   },
 ];
@@ -124,11 +113,13 @@ export function Services() {
 
         <div className="mt-12 grid grid-cols-1 md:grid-cols-3 gap-8">
           {services.map(({ id, icon, title, description }: Service) => (
-            <div key={id} className="flex justify-centerccc">
-              <div className="bg-background text-foreground p-8 shadow-lg rounded-lg hover:shadow-2xl transition border border-border cursor-pointer">
+            <div key={id} className="flex justify-center">
+              <div className="bg-background text-foreground shadow-lg rounded-lg hover:shadow-2xl transition border border-border cursor-pointer overflow-hidden">
                 <div className="flex justify-center mb-6">{icon}</div>
-                <h3 className="text-xl font-semibold">{title}</h3>
-                <p className="text-muted-foreground mt-4">{description}</p>
+                <div className="px-6 pb-6">
+                  <h3 className="text-xl font-semibold">{title}</h3>
+                  <p className="text-muted-foreground mt-4">{description}</p>
+                </div>
               </div>
             </div>
           ))}
